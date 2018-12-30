7baf2eb0948d6a681e573ac7a5695da1c4a5a5ca
Théo <theo.lieffroy@utbm.fr>
2018-12-30 19:35:05 +0100

    test

---
+++ structure.h
@@ -0,0 +1,10 @@
+#ifndef STRUCTURE_H_INCLUDED
+#define STRUCTURE_H_INCLUDED
+#include "constante.h"
+
+typedef struct Individu
+{
+    unsigned int bit[longIndiv];
+}
+
+#endif // STRUCTURE_H_INCLUDED
